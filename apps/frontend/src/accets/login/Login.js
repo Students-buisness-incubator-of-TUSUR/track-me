23364ee8e606aaf30b1f8a4546b725dd734b856f
NVA1337 <kek2004228@mail.ru>
2025-02-17 16:56:55 +0700

    попробуем так

--- apps/frontend/src/accets/login/Login.js
+++ apps/frontend/src/accets/login/Login.js
@@ -90,8 +90,8 @@ const Login = () => {
               required
             />
           </div>
-          
-          {/* <div className="input-container">
+
+          <div className="input-container">
             <label className="login-label" htmlFor="password">Пароль</label>
             <div className="password-container">
               <input
@@ -113,9 +113,9 @@ const Login = () => {
                 style={{ cursor: "pointer" }}
               />
             </div>
-          </div> */}
+          </div>
 
-          <a href="#" className="forgot-password">
+          <a href="#" className="forgot-password" onClick={handleRegisterClick}>
             Забыли пароль? Восстановить?
           </a>
 
