4e1e6be937d8b3b53d0f1903de72b9d6544e609a
KiriLLobZor <teplyakovkirill0@gmail.com>
2025-02-17 14:14:09 +0700

    Adding a registration
    Добавил исправную регистрацию
    с возможностью зарегистрироваться админом в панели

--- apps/frontend/src/App.js
+++ apps/frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Registration from "./accets/register/Registration.js";
 import Login from "./accets/Login.js";
 import Stream from "./accets/stream-page/stream-page.js";
 
-const Register = () => <h1>Страница регистрации</h1>;
+//const Register = () => <h1>Страница регистрации</h1>;
 const AdminPage = () => <h1>Страница Админа</h1>;
 const SuperAdminPage = () => <h1>Страница Суперадмина</h1>;
 const TrackerPage = () => <h1>Страница Трекера</h1>;
@@ -15,7 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/stream" element={<Stream />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/register" element={<Registration />} />
         <Route path="/admin" element={<AdminPage />} />
         <Route path="/superadmin" element={<SuperAdminPage />} />
         <Route path="/tracker" element={<TrackerPage />} />
