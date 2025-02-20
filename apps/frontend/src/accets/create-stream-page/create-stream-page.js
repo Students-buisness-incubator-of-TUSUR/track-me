1a0630feb0e75a58089524c820ecc75d4e657816
NVA1337 <kek2004228@mail.ru>
2025-02-20 12:26:57 +0700

    созданик страницы создания потоков

---
+++ apps/frontend/src/accets/create-stream-page/create-stream-page.js
@@ -0,0 +1,10 @@
+// import React, { useState } from 'react';
+// import { Link } from "react-router-dom";
+import './create-stream-page.css';
+export default function Create_stream() {
+  return (
+    <div className="Create-stream">
+
+    </div>
+  );
+}
