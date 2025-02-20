a52397a632551cfcfc6cbba3056ebb1a1af7081a
NVA1337 <kek2004228@mail.ru>
2025-02-20 13:36:50 +0700

    Создание новой страницы для создания потоков

--- apps/frontend/src/App.js
+++ apps/frontend/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./accets/login/Login.js";
 import Stream from "./accets/stream-page/stream-page.js";
-import Create_stream from "./accets/create-stream-page/create-stream-page.js";
+import CreateStream from "./accets/create-stream-page/create-stream-page.js";
 
 import TrackerPage from "./accets/teamcard/TrackerPage.js";
 import Register from "./accets/register/Registration.js";
@@ -22,7 +22,7 @@ function App() {
         <Route path="/admin" element={<AdminPage />} />
         <Route path="/superadmin" element={<SuperAdminPage />} />
         <Route path="/tracker" element={<TrackerPage />} />
-        <Route path="/create-stream" element={<Create_stream />} />
+        <Route path="/create-stream" element={<CreateStream />} />
         <Route path="/profile" element={<ProfilePage />} />
       </Routes>
     </Router>
