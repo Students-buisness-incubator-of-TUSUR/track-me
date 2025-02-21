097a9cebb74b96d9c08554f54792f7ba7732a078
KiriLLobZor <teplyakovkirill0@gmail.com>
2025-02-21 14:36:46 +0700

    карта встречи

--- apps/frontend/src/App.js
+++ apps/frontend/src/App.js
@@ -6,6 +6,7 @@ import Stream from "./accets/stream-page/stream-page.js";
 import TrackerPage from "./accets/teamcard/TrackerPage.js";
 import Register from "./accets/register/Registration.js";
 import ProfilePage from "./accets/profile/ProfilePage.js"; // <-- наш компонент
+import MeetingCard from "./accets/meeting-card/meeting-card.js";
 
 const AdminPage = () => <h1>Страница Админа</h1>;
 const SuperAdminPage = () => <h1>Страница Суперадмина</h1>;
@@ -20,8 +21,8 @@ function App() {
         <Route path="/admin" element={<AdminPage />} />
         <Route path="/superadmin" element={<SuperAdminPage />} />
         <Route path="/tracker" element={<TrackerPage />} />
-        
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/meeting" element={<MeetingCard />} />
+        <Route path="/ " element={<ProfilePage />} />
       </Routes>
     </Router>
   );
