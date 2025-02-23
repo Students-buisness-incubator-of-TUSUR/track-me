adb5dfc48953ee08ecd125420bd93f1e9464ade3
akarmanov <mrmorris853@gmail.com>
2025-02-23 10:26:34 +0700

    Refactor user photo handling: update image format to PNG, add validation for file size and type, and improve user photo retrieval logic

--- apps/backend/src/main/java/net/akarmanov/projectplace/domain/User.java
+++ apps/backend/src/main/java/net/akarmanov/projectplace/domain/User.java
@@ -104,7 +104,7 @@ public class User implements UserDetails {
 
   @OneToMany(mappedBy = "user",
              cascade = CascadeType.ALL)
-  private Set<TeamCard> userTeamCards;
+  private Set<TeamCard> userTeamCards = new HashSet<>();
 
   @ManyToMany
   @JoinTable(
