4315c37f3f55c1e6fdfe4ac63485a85713a6adb4
NVA1337 <kek2004228@mail.ru>
2025-03-10 08:49:05 +0700

    добавил корректные запросы в списках команд

--- apps/frontend/src/accets/stream-page/stream-page.js
+++ apps/frontend/src/accets/stream-page/stream-page.js
@@ -20,7 +20,7 @@ export default function Stream() {
   const [searchQuery, setSearchQuery] = useState('');
   // eslint-disable-next-line
   const [filters, setFilters] = useState([]); // Состояние для фильтров
-  const [page, setpage] = useState(0); 
+  const [page, setpage] = useState(1); 
   const [selectedYears, setSelectedYears] = useState(new Set()); // Выбранные годы (Set)
 
   const [selectedMarkets, setSelectedMarkets] = useState(new Set()); // Выбранные рынки (Set)
@@ -37,6 +37,8 @@ export default function Stream() {
     setLoading(true);
     setError(null);
     console.log(page);
+
+    
     const token = localStorage.getItem("accessToken");
     if (!token) {
       setError("Ошибка: отсутствует токен авторизации. Выполните вход.");
@@ -84,11 +86,13 @@ export default function Stream() {
   };
 
   const handleShowMore = () => {
+    console.log(data.page.totalPages);
     setpage(page+1);
 
   };
 
   const handleShowPrevious = () => {
+    console.log(data.page.totalPages);
     setpage(page-1);
   };
 
@@ -401,21 +405,21 @@ export default function Stream() {
       <footer className="Stream-footer">
         <div className="Stream-footer-butts">
           <div className="Stream-footer-p-butt-1">
-            {data.page.totalPages -1 <= page && (
+            {1 < page && (
               <button onClick={handleShowPrevious} className="Stream-footer-button-1"></button>
             )}
           </div>
           <div className="Stream-footer-p-butts">
-            {data.page.totalPages -1 <= page && (
+            {1 < page && (
               <button onClick={handleShowPrevious} className="Stream-footer-button-2"></button>
             )}
             <button className="Stream-footer-button-3"></button>
-            {data.page.totalPages -1 > page && (
+            {data.page.totalPages > (page + 1) && (
               <button onClick={handleShowMore} className="Stream-footer-button-4"></button>
             )}
           </div>
           <div className="Stream-footer-p-butt-5">
-            {data.page.totalPages -1 > page && (
+            {data.page.totalPages > (page + 1) && (
               <button onClick={handleShowMore} className="Stream-footer-button-5"></button>
             )}
           </div>
