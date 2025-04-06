5cc9ffca4230f4b937193c405f66d14b1b2a7170
akarmanov <mrmorris853@gmail.com>
2025-04-06 12:35:39 +0700

    Refactor project structure, update application configuration, and enhance OpenAPI integration

--- services/trackme-sso/src/main/java/net/akarmanov/projectplace/sso/config/AppConfiguration.java
+++ services/trackme-sso/src/main/java/net/akarmanov/projectplace/sso/config/AppConfiguration.java
@@ -2,20 +2,26 @@ package net.akarmanov.projectplace.sso.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableJpaRepositories(basePackages = "net.akarmanov.projectplace.sso.dao.repository")
 public class AppConfiguration {
+
+  private final BuildProperties buildProperties;
+
   @Bean
   OpenAPI openApi() {
     return new OpenAPI()
         .info(
             new Info()
                 .title("TrackMe SSO Service")
-                .version("1.0.0")
-                .description("Single Sign-On service for TrackMe application"));
+                .version(buildProperties.getVersion())
+                .description("Единая точка входа для всех сервисов системы TrackMe"));
   }
 }
