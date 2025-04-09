6c5bc9fa0aebd8822546b148069e07dd446aeebc
akarmanov <mrmorris853@gmail.com>
2025-04-09 22:23:57 +0700

    Add CORS configuration, update API paths, and create development environment file

---
+++ apps/trackme-gateway/src/main/java/net/akarmanov/projectplace/cliengateway/config/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package net.akarmanov.projectplace.cliengateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class SecurityConfiguration {
+  @Bean
+  public CorsWebFilter corsWebFilter() {
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowedOrigins(List.of(CorsConfiguration.ALL)); // или "*" для тестов
+    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    config.setAllowedHeaders(List.of("*"));
+    config.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config);
+
+    return new CorsWebFilter(source);
+  }
+}
