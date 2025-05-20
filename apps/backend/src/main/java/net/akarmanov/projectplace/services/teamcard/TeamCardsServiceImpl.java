15886b0a62faa812d563427bcc45b54cec5f3775
akarmanov <mrmorris853@gmail.com>
2025-05-20 14:53:40 +0700

    Make 'username' optional in updateTeamCard API and service
    Updated the `updateTeamCard` method in both the controller and service to handle `username` as an optional parameter. Adjusted logic to set the username only when provided. Added a placeholder for a dev container configuration file.

--- apps/backend/src/main/java/net/akarmanov/projectplace/services/teamcard/TeamCardsServiceImpl.java
+++ apps/backend/src/main/java/net/akarmanov/projectplace/services/teamcard/TeamCardsServiceImpl.java
@@ -84,13 +84,16 @@ public class TeamCardsServiceImpl implements TeamCardsService {
     @PreAuthorize("hasRole('ADMIN')")
     public TeamCard updateTeamCard(UUID teamCardId,
                                    TeamCard teamCardDto,
-                                   UUID streamId, String username) {
-        var teamCard = get(teamCardId, username);
+                                   UUID streamId,
+                                   String username) {
+        var teamCard = get(teamCardId);
         updateTeamCard(teamCardDto, teamCard);
-        teamCard.setUsername(username);
         if (streamId != null) {
             var stream = streamService.getById(streamId);
             teamCard.addStream(stream);
+        }
+        if (username != null) {
+            teamCard.setUsername(username);
         }
         return teamCardsRepository.save(teamCard);
     }
