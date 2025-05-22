30b9bd5b3a59993a8e7b88de918ac722811a1422
akarmanov <mrmorris853@gmail.com>
2025-05-22 17:54:46 +0700

    Add initial setup for Trackme Report Service
    Introduces the Trackme Report Service as a Spring Boot application, including its build configuration, Dockerfile, and test setup. Configured Gradle dependencies, application properties, and added the module to the project structure.

---
+++ services/trackme-report-service/src/main/java/net/akarmanov/projectplace/trackmereportservice/TrackmeReportServiceApplication.java
@@ -0,0 +1,13 @@
+package net.akarmanov.projectplace.trackmereportservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackmeReportServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackmeReportServiceApplication.class, args);
+    }
+
+}
