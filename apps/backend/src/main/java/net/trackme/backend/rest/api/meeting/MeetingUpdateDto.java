ab61000102c17b7565c9daabf88a9ef922ff4f01
akarmanov <mrmorris853@gmail.com>
2025-07-01 14:52:37 +0700

    Refactor NTI market service and update meeting DTO validation
    Removed unused method `getNtiMarket` and updated `getNtiMarkets` to throw an exception when no markets are found. Modified `NtiMarketNotFoundException` to handle lists of IDs. Simplified validation for meeting DTOs by removing URL pattern checks and allowing nullable fields.

--- apps/backend/src/main/java/net/trackme/backend/rest/api/meeting/MeetingUpdateDto.java
+++ apps/backend/src/main/java/net/trackme/backend/rest/api/meeting/MeetingUpdateDto.java
@@ -1,17 +1,15 @@
 package net.trackme.backend.rest.api.meeting;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Pattern;
+import jakarta.annotation.Nullable;
 import lombok.Builder;
 import net.trackme.backend.models.MeetingStatus;
 
 @Builder
 @Schema(description = "DTO обновления встречи команды")
 public record MeetingUpdateDto(
+        @Nullable
         @Schema(description = "Ссылка на встречу")
-        @Pattern(regexp = "^(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})([/\\w .-]*)*/?$",
-                message = "Ссылка должна быть в правильном формате URL",
-                flags = Pattern.Flag.CASE_INSENSITIVE)
         String link,
         @Schema(description = "Номер встречи")
         String number,
