d4f409e62b89aff36417cb05e0ece7156f38f52a
MelnikovS5 <semenmelnikov87@gmail.com>
2025-11-09 06:41:29 +0700

    тест

--- apps/frontend/src/accets/komand/team-card.js
+++ apps/frontend/src/accets/komand/team-card.js
@@ -877,7 +877,7 @@ const saveMeetingDate = async () => {
             </div>
 
             <div className="right-panel">
-  <button className="show-meetings-mobile-btn" onClick={() => setShowMeetingsOnMobile(!showMeetingsOnMobile)}>
+  <button className="show-meetings-mobile-btn" onClick={() => setShowMeetingsOnMobile(!showMeetingsOnMobile)}>{/*NOSONAR*/}
                 {showMeetingsOnMobile ? "Скрыть встречи" : "Показать встречи"}
               </button>
               {/* Сообщение об ошибке */}
