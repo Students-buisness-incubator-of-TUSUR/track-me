e5e58eed47410b1d6bf968864bd26fbc37974edd
Ivan Lifanov <letalbark@gmail.com>
2026-02-11 15:54:41 +0700

    добавлен компонент InputBox являющийся input элементом с доп. фичами текста ошибки и placeholder, переходящий наверх

---
+++ services/trackme-sso/frontend/src/assets/input-box/InputBox.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import "./InputBox.css";
+
+const InputBox = ({ placeholder, type, autoComplete, name, value, onChange, required, pattern, errorText }) => {
+    const [isFocused, setIsFocused] = useState(false);
+
+    return (
+        <div className="input-box_container-with-helper">
+            <div className="input-box_container">
+                <input
+                    type={type}
+                    className=""
+                    name={name}
+                    value={value}
+                    onChange={onChange}
+                    required={required}
+                    pattern={pattern}
+                    onFocus={() => setIsFocused(true)}
+                    onBlur={() => setIsFocused(false)}
+                    autoComplete={autoComplete}
+                    placeholder="fake" // input is non empty if placeholder is not shown for CSS
+                />
+                <label>{placeholder}</label>
+            </div>
+            <span>{isFocused ? errorText : (value !== "" ? errorText : "")}</span>
+        </div>
+    );
+};
+
+export default InputBox;
