e2488e44f02284678f5ac3edfbeebff5e47959d5
trackme-backend <trackme.backend@gmail.com>
2026-02-24 15:52:11 +0700

    Добавлен атрибут "Ссылка на комнату для встреч" к команде.

--- apps/backend/src/main/java/net/trackme/backend/rest/api/teamcard/dto/TeamCardCreateOrUpdateDto.java
+++ apps/backend/src/main/java/net/trackme/backend/rest/api/teamcard/dto/TeamCardCreateOrUpdateDto.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
+import org.hibernate.validator.constraints.URL;
 
 import java.util.List;
 import java.util.UUID;
@@ -12,22 +13,38 @@ import java.util.UUID;
 @Schema(description = "DTO для создания/обновления карточки команды")
 public record TeamCardCreateOrUpdateDto(
         @NotBlank(message = "Название карточки команды не может быть пустым")
-        @Schema(description = "Название карточки команды",
-                example = "Карточка команды")
+        @Schema(
+                description = "Название карточки команды",
+                example = "Карточка команды"
+        )
         String name,
 
-        @Schema(description = "Описание карточки команды",
-                example = "Описание карточки команды")
+        @Schema(
+                description = "Описание карточки команды",
+                example = "Описание карточки команды"
+        )
         String description,
+
+        @NotBlank(message = "Ссылка на комнату для встреч не может быть пустой")
+        @URL(message = "Ссылка должна быть корректным URL (например: https://webinar.tusur.ru/b/jm7-p47-8j8-3ib)")
+        @Schema(
+                description = "Ссылка на комнату для встреч",
+                example = "https://webinar.tusur.ru/b/jm7-p47-8j8-3ib"
+        )
+        String meetingRoomLink,
 
         @Schema(description = "Идентификаторы рынков НТИ, к которым относится карточка команды")
         List<UUID> ntiMarketIds,
 
-        @Schema(description = "Уровень готовности технологии",
-                allowableValues = {"0-2", "3-5", "6-8", "9-10"})
         @NotBlank(message = "Уровень готовности технологии не может быть пустым")
-        @Pattern(regexp = "0-2|3-5|6-8|9-10",
-                message = "Уровень готовности технологии должен быть одним из значений: 0-2, 3-5, 6-8, 9-10")
+        @Schema(
+                description = "Уровень готовности технологии",
+                allowableValues = {"0-2", "3-5", "6-8", "9-10"}
+        )
+        @Pattern(
+                regexp = "0-2|3-5|6-8|9-10",
+                message = "Уровень готовности технологии должен быть одним из значений: 0-2, 3-5, 6-8, 9-10"
+        )
         String readinessLevel
 ) {
 }
