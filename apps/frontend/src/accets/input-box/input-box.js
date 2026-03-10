4b3850f43be59a66828bcc046e46ff267201a172
Ivan Lifanov <letalbark@gmail.com>
2026-03-10 19:34:04 +0700

    add input-box component with an option to show placeholder above input

---
+++ apps/frontend/src/accets/input-box/input-box.js
@@ -0,0 +1,57 @@
+import React, { useId, useState } from "react";
+import "./input-box.css";
+import { ReactComponent as PenIcon } from '../../files/pen.svg';
+
+const InputBox = ({
+    placeholder,
+    placeholderIsAbove = false,
+    autoComplete,
+    errorText,
+    name,
+    type,
+    value,
+    onEditClick = null,
+    ...props
+}) => {
+    const [isFocused, setIsFocused] = useState(false);
+    const id = useId();
+
+    return (
+        <div className="input-box_container-with-helper">
+            {placeholderIsAbove && (
+                <label className="input-box_label-above" htmlFor={id}>
+                    {placeholder}
+                </label>
+            )}
+            <div
+                className={`input-box_container${placeholderIsAbove ? " input-box_container--above" : ""}`}
+            >
+                <input
+                    id={id}
+                    onFocus={() => setIsFocused(true)}
+                    onBlur={() => setIsFocused(false)}
+                    placeholder="fake"
+                    name={name}
+                    type={type}
+                    value={value}
+                    {...props}
+                />
+                {!placeholderIsAbove && <label htmlFor={id}>{placeholder}</label>}
+                {onEditClick && (
+                    <button
+                        type="button"
+                        onClick={onEditClick}
+                    >
+                        <PenIcon/>
+                    </button>
+                )}
+            </div>
+            <span>
+                {isFocused ? errorText : value !== "" ? errorText : ""}
+            </span>
+        </div>
+    );
+};
+
+export default InputBox;
+
