aa258469b1c8a2d63d84ad345b1f5402be7398c9
MelnikovS5 <semenmelnikov87@gmail.com>
2026-03-26 12:24:25 +0700

    доработки

--- apps/frontend/src/accets/listuser/TrackerListPage.js
+++ apps/frontend/src/accets/listuser/TrackerListPage.js
@@ -53,10 +53,10 @@ function TrackerListPage({ endpoint }) {
 
 
   // Закрыть меню при клике вне элемента
-  // const closeMobileMenu = () => {
-  //   setActiveMobileMenu(null);
-  //   setHoveredTracker(null);
-  // };
+  const closeMobileMenu = () => {
+    setActiveMobileMenu(null);
+    setHoveredTracker(null);
+  };
 
   const handleDoubleTap = (username) => {
   const now = Date.now(); // Текущее время в миллисекундах
@@ -150,14 +150,19 @@ function TrackerListPage({ endpoint }) {
         </div>
         </div>
      <main 
-  className="tracker-list-content" 
-  // onClick={closeMobileMenu}
-  // onKeyDown={(e) => {
-  //   if (e.key === 'Escape' || e.key === 'Enter' || e.key === ' ') {
-  //     closeMobileMenu();
-  //   }
-  // }}
-  aria-label="Close mobile menu" // Provide accessible description
+      className="tracker-list-content" 
+      onClick={closeMobileMenu}
+      onKeyDown={(e) => {
+        if (e.key === 'Enter' || e.key === ' ') {
+          e.preventDefault();
+          closeMobileMenu();
+        }
+        if (e.key === 'Escape') {
+          closeMobileMenu();
+        }
+      }}
+      tabIndex={-1}
+      aria-label="Close mobile menu"
 >
   {error && <div className="error-message oval2">{error}</div>}
 
