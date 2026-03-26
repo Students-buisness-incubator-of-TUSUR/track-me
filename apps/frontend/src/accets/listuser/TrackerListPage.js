37f2a055a65e96881120ab1838cb4f63485681d4
MelnikovS5 <semenmelnikov87@gmail.com>
2026-03-26 11:22:19 +0700

    Update TrackerListPage.js

--- apps/frontend/src/accets/listuser/TrackerListPage.js
+++ apps/frontend/src/accets/listuser/TrackerListPage.js
@@ -76,7 +76,7 @@ function TrackerListPage({ endpoint }) {
     }
 
     // В любом случае — переходим в профиль
-    globalThis.location.href = `/profile/${username}`;
+    window.location.href = `/profile/${username}`;
   } else {
     // Это одиночное касание — ставим таймер
     tapTimeout.current = setTimeout(() => {
@@ -188,7 +188,7 @@ function TrackerListPage({ endpoint }) {
               if (!isMobile()) {
                 // Разрешаем переход, только если клик НЕ по панели
                 if (!e.target.closest('.tracker-edit-panel12')) {
-                  globalThis.location.href = `/profile/${tracker.username}`;
+                  window.location.href = `/profile/${tracker.username}`;
                 }
               }
             }}
