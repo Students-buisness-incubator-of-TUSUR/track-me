2e596eaa8c3c41498c1e1e7b30bec2890a766e7c
emma klatscht <51996726+tr1ppy9in3@users.noreply.github.com>
2026-04-01 17:21:27 +0700

    Добавлены тесты событий на SSO.

--- apps/backend/src/test/java/net/trackme/backend/messaging/TeamCardSyncFlowTest.java
+++ apps/backend/src/test/java/net/trackme/backend/messaging/TeamCardSyncFlowTest.java
@@ -1,6 +1,6 @@
-package net.trackme.backend.services.teamcard;
+package net.trackme.backend.messaging;
 
-import net.trackme.backend.messaging.*;
+import net.trackme.backend.services.teamcard.TeamCardEventsProducer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
