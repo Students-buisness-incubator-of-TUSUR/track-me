c2b8cdeb1c89e845c16c56ac893d50136181f503
BDA228 <131526143+BDA228@users.noreply.github.com>
2026-04-28 18:01:58 +0700

    Изменил название

--- apps/frontend/src/accets/meeting-card/meeting-card.js
+++ apps/frontend/src/accets/meeting-card/meeting-card.js
@@ -535,7 +535,7 @@ const MeetingCard = () => {
                                 className="unique-date-input"
                                 disabled={isMeetingLocked}
                                 min={new Date().toISOString().split('T')[0]}
-                            />
+                            /> 
                             <img src={pencilIcon} alt="Редактировать" style={{ marginTop: "-6px" }} className="edit-icon23" />
                         </div>
                     ) : (
