b901ff0f48e18a4aa2ba75e931309126bea98efe
lenale7 <elenao5343@icloud.com>
2026-05-21 17:33:40 +0700

    Add passive status tests; restore original test files from develop

--- apps/frontend/src/accets/teamcard/tracker-page.test.js
+++ apps/frontend/src/accets/teamcard/tracker-page.test.js
@@ -1,3 +1,5 @@
+// src/accets/teamcard/TrackerPage.test.js
+
 import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
